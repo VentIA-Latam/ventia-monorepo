0c199437e237b4753db53a835927df41f001dfbb
Renzo Lenes <lenesrenzoalberto@gmail.com>
2026-03-24 14:06:36 -0500

    fix: update SW Firebase SDK to 11.6.0 to match frontend version

--- apps/frontend/public/firebase-messaging-sw.js
+++ apps/frontend/public/firebase-messaging-sw.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-undef */
-importScripts("https://www.gstatic.com/firebasejs/10.14.1/firebase-app-compat.js");
-importScripts("https://www.gstatic.com/firebasejs/10.14.1/firebase-messaging-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/11.6.0/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/11.6.0/firebase-messaging-compat.js");
 
 firebase.initializeApp({
   apiKey: "AIzaSyCeT7BBrfRklxgq_6yVLjePdblrQ1aUkMw",
